28f697a477cd395a70457307fa5595e89b46c7e1
Dayron-z <104926781+Dayron-z@users.noreply.github.com>
2024-04-29 18:10:54 -0500

    Se crea estructura de carpetas

---
+++ belleza/src/main/java/com/centro/belleza/domain/entities/Servicio.java
@@ -0,0 +1,14 @@
+package com.centro.belleza.domain.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "servicios")
+public class Servicio {
+    private int id;
+
+
+
+}
---
+++ belleza/src/test/java/com/centro/belleza/BellezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centro.belleza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
